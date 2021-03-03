3aca55b904660986ac1ef7d65080e2d065bb9d89
Arefin <60544231+Arefin12345@users.noreply.github.com>
2021-03-03 13:13:12 +0600

    my first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import ReactDOM from 'react-dom';
+
+var name = 12;
+const fname = "arefin";
+const lname = "ahmed";
+ReactDOM.render(<>
+{/* This works as div , doesn create extra node */}
+   
+   
+    {     
+        /* // <    
+            // h1 > My name is arefin ahmed < /h1>  <
+            // p > Who are you < /p>    <
+            // h2 > Please put some water. < /h2>
+            // <  */
+    }
+      
+    {/* <h1> My name is {fname + lname }</h1> */}
+    <h1> {`My name is ${fname} ${lname}`} </h1>
+    <h1> My lucky number is {5+5} </h1>
+</> ,
+    
+    // var fname = "arefin";
+
+    // ReactDOM.render(,
+
+    //     document.getElementById("root");
+    // );
+
+    // [ <
+    //     h1 > My name is arefin ahmed < /h1>, <
+    //     p > Who are you < /p> , <
+    //     h2 > Please put some water. < /h2>
+    // ],
+    // div > 
+    // <
+    // h1 > My name is arefin ahmed < /h1> <
+    // p > Who are you < /p> <
+    // h2 > Please put some water. < /h2> <
+    // /div> ,
+    document.getElementById('root')
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "I am H1";
+// document.getElementById("root").appendChild(h1);
---
+++ src/javascriptInJsX.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+const fname = "arefin";
+
+ReactDOM.render( <
+    h1 >
+    My name is { fname }
+
+    <
+    /h1>,
+    document.getElementById("root")
+);
